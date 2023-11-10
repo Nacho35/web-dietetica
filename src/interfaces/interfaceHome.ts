85fbc9c4ae44b1ea501405f432b0367fc2230f9a
Ignacio Morales <103209579+Nacho35@users.noreply.github.com>
2023-11-10 17:59:41 -0300

    creando el home y la navbar

---
+++ src/interfaces/interfaceHome.ts
@@ -0,0 +1,5 @@
+interface Slide {
+	image: string;
+	title: string;
+	text: string;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './App.css';
-import 'materialize-css/dist/css/materialize.min.css';
-import 'materialize-css/dist/js/materialize.min.js';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
