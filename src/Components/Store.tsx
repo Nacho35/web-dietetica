60c10e4f5e608ee708703027b4fbde4f94214d20
Admin <ignacioevil@gmail.com>
2024-03-05 22:58:38 -0300

    componente store terminado

--- src/Components/Store.tsx
+++ src/Components/Store.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Slider from 'react-slick';
 import {
 	Container,
@@ -6,6 +7,10 @@ import {
 	Title,
 	PrimaryTitle,
 	GlobalContainer,
+	ContainerImage,
+	DescriptionOverlay,
+	Button,
+	ContainerBTN,
 } from '../styles/styledStore.ts';
 import fruits from '../assets/fruits.webp';
 import eco from '../assets/eco.webp';
@@ -13,14 +18,61 @@ import food from '../assets/food.webp';
 import drinks from '../assets/drinks.webp';
 import bags from '../assets/bags.webp';
 import { Slides } from '../interfaces/interfaceStore';
+import left from '../assets/nav-arrow-left.svg';
+import right from '../assets/nav-arrow-right.svg';
+
+const NextArrow = (props: any) => (
+	<div className='arrow-left' onClick={props.onClick}>
+		<img src={left} alt='arrow' />
+	</div>
+);
+
+const PrevArrow = (props: any) => (
+	<div className='arrow-right' onClick={props.onClick}>
+		<img src={right} alt='arrow' />
+	</div>
+);
 
 const Store = () => {
+	const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
 	const settings = {
 		dots: true,
 		infinite: true,
 		speed: 500,
-		slidesToShow: 1,
+		slidesToShow: 4,
 		slidesToScroll: 1,
+		nextArrow: <NextArrow />,
+		prevArrow: <PrevArrow />,
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: true,
+				},
+			},
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: true,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: true,
+				},
+			},
+		],
 	};
 
 	const slides: Slides[] = [
@@ -48,17 +100,30 @@ const Store = () => {
 
 	return (
 		<Container>
-			<PrimaryTitle>Our Products</PrimaryTitle>
+			<PrimaryTitle>Earth's Bounty: Organic Wonders Await</PrimaryTitle>
 			<GlobalContainer>
 				<Slider {...settings}>
 					{slides.map((slide, index) => (
-						<InnerContainer key={index}>
-							<Image src={slide.image} alt={slide.title} />
-							<Title>{slide.title}</Title>
+						<InnerContainer
+							key={index}
+							onMouseEnter={() => setHoveredIndex(index)}
+							onMouseLeave={() => setHoveredIndex(null)}
+						>
+							<ContainerImage>
+								<Image src={slide.image} alt={slide.title} />
+								{hoveredIndex === index && (
+									<DescriptionOverlay>
+										<Title>{slide.title}</Title>
+									</DescriptionOverlay>
+								)}
+							</ContainerImage>
 						</InnerContainer>
 					))}
 				</Slider>
 			</GlobalContainer>
+			<ContainerBTN>
+				<Button>see products</Button>
+			</ContainerBTN>
 		</Container>
 	);
 };
--- src/styles/styledStore.ts
+++ src/styles/styledStore.ts
@@ -13,7 +13,15 @@ export const Container = styled.div`
 export const PrimaryTitle = styled.h2`
 	text-transform: uppercase;
 	text-align: center;
-	margin-top: 1rem;
+	align-self: center;
+	padding: 2rem;
+	font-size: 20px;
+	border-radius: 10px;
+	width: fit-content;
+	border-radius: 100px 10px;
+	background: rgba(255, 255, 255, 0.2);
+	backdrop-filter: blur(10px);
+	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 `;
 
 export const GlobalContainer = styled.div`
@@ -25,15 +33,78 @@ export const InnerContainer = styled.div`
 	flex-direction: column;
 	justify-content: center;
 	height: auto;
+	position: relative;
+	overflow: hidden;
 `;
 
 export const Image = styled.img`
 	width: 100%;
 	height: auto;
 	border-radius: 10px;
+	display: block;
+`;
+
+export const ContainerImage = styled.div`
+	margin: 10px;
 `;
 
 export const Title = styled.h2`
 	text-align: center;
 	text-transform: capitalize;
+	font-size: 1rem;
+`;
+
+export const DescriptionOverlay = styled.div`
+	position: absolute;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	color: ${theme.colors.colorHeight};
+	text-align: center;
+	border-radius: 10px 100px;
+	background: rgba(255, 255, 255, 0.2);
+	backdrop-filter: blur(10px);
+	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+	opacity: 0;
+	transition: opacity 0.3s ease-in-out;
+
+	${ContainerImage}:hover & {
+		opacity: 1;
+	}
+`;
+
+export const Button = styled.button`
+	margin: auto;
+	text-transform: capitalize;
+	width: fit-content;
+	font-weight: 600;
+	position: relative;
+	display: inline-block;
+	margin: 15px;
+	padding: 15px 30px;
+	text-align: center;
+	font-size: 18px;
+	letter-spacing: 1px;
+	text-decoration: none;
+	color: ${theme.colors.colorHeight};
+	cursor: pointer;
+	transition: ease-out 0.5s;
+	border-radius: 10px;
+	background: rgba(255, 255, 255, 0.2);
+	backdrop-filter: blur(10px);
+	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+
+	&:hover {
+		color: ${theme.colors.colorSeven};
+		box-shadow: inset 0 -100px 0 0 ${theme.colors.colorHeight};
+	}
+
+	&:active {
+		transform: scale(0.9);
+	}
+`;
+
+export const ContainerBTN = styled.div`
+	margin-top: 1.2rem;
+	align-self: center;
 `;
